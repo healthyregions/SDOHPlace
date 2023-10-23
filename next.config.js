a1aea045b578aa25647b3998c01832ade5f4de6d
Adam Cox <mr.adamcox@gmail.com>
2023-10-23 16:35:05 -0500

    feat: initial decap cms integration

--- next.config.js
+++ next.config.js
@@ -2,4 +2,23 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ["tsx"],
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          use: "yaml-loader",
+        },
+        // {
+        //   test: /\.svg$/,
+        //   use: "@svgr/webpack",
+        // },
+      ]
+    );
+    return config;
+  },
 };
