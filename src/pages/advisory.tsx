4d1e4b51358ef38151cf8b2d20b28092df470f3c
Adam Cox <mr.adamcox@gmail.com>
2023-10-26 13:18:45 -0500

    fix: initial stab at blog styling #14

--- src/pages/advisory.tsx
+++ src/pages/advisory.tsx
@@ -44,13 +44,6 @@ const modalBtnStyle = {
 
 function Advisory(props) {
   const data = {
-    advisory: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum`,
     team: {
       stakeholders: [
         {
@@ -292,7 +285,21 @@ function Advisory(props) {
             <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
               <div className="flex flex-col items-stretch w-[92%] max-md:w-full max-md:ml-0">
                 <div className="text-stone-900 text-2xl leading-[133.333%] w-[1068px] max-w-[1068px] max-md:max-w-full max-md:mt-10">
-                  {data.advisory}
+                  The Place project has an advisory board consisting of experts
+                  from public, private, and academic sectors. Their primary
+                  responsibility is to provide methodological guidance, offer
+                  critical and technical insights, and ensure that the PLACE
+                  project aligns with the requirements of communities in the
+                  United States that are disproportionately affected by health,
+                  spatial, and racial disparities.
+                  <br />
+                  <br />
+                  The members of the PLACE project advisory board combine vast
+                  experience in designing complex engineering and scientific
+                  solutions for both non-profit and academic settings, as well
+                  as a proven track record in conducting community-based
+                  research and outreach programs with minority and segregated
+                  communities.
                 </div>
               </div>
             </div>
