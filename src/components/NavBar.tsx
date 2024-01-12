90bcfb1569fd6e24f3c6b823fa23b74d6e33e639
Adam Cox <mr.adamcox@gmail.com>
2024-01-11 18:41:13 -0600

    style: mobile menu style #16

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -31,9 +31,7 @@ const NavBar = (): JSX.Element => {
       className={`absolute left-0 top-0 w-full z-50 ease-in duration-300 bg-${navBackgroundColor}`}
     >
       <div
-        className={`flex ${
-          nav ? "justify-end" : "justify-between"
-        } items-center 2xl:max-w-[1536px] pt-8 pb-12 pl-0 pr-0 mx-auto`}
+        className={`flex justify-between items-center 2xl:max-w-[1536px] pt-8 pb-12 pl-0 pr-0 mx-auto`}
       >
         <ul className="hidden min-[768px]:flex pl-[2.5%]">
           <li
@@ -76,9 +74,9 @@ const NavBar = (): JSX.Element => {
         {/* Mobile Button */}
         <div
           onClick={handleNav}
-          className="block min-[768px]:hidden pl-[3%] z-50"
+          className="block min-[768px]:hidden pl-[25px] z-50"
         >
-          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+          {nav ? <AiOutlineClose size={35} /> : <AiOutlineMenu size={35} />}
         </div>
 
         {/* Mobile Menu */}
