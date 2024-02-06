5339ac5925c98186d62cead783dbf230f48c4e57
Adam Cox <mr.adamcox@gmail.com>
2024-02-06 11:05:41 -0600

    feat: #77 add initial draft of map component, with pmtiles sources for all boundaries

---
+++ src/components/map/sources.tsx
@@ -0,0 +1,21 @@
+// these are the sources for all overlay layers. multiple layers can be added to the map
+// that use the same source: for example a source for state boundaries can be displayed as a
+// polygon (fill) layer as well as a line (outline) layer
+export const sources = {
+  state: {
+    type: "vector",
+    url: "pmtiles://https://herop-geodata.s3.us-east-2.amazonaws.com/state-2018.pmtiles",
+  },
+  county: {
+    type: "vector",
+    url: "pmtiles://https://herop-geodata.s3.us-east-2.amazonaws.com/county-2018.pmtiles",
+  },
+  tract: {
+    type: "vector",
+    url: "pmtiles://https://herop-geodata.s3.us-east-2.amazonaws.com/tract-2018.pmtiles",
+  },
+  bg: {
+    type: "vector",
+    url: "pmtiles://https://herop-geodata.s3.us-east-2.amazonaws.com/bg-2018.pmtiles",
+  },
+};
