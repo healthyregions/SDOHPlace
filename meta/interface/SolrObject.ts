da771da7725fa9b2e3ea03ebb6c5bd7f0e0c9292
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2024-03-07 18:02:11 -0600

    test editorconfig

--- meta/interface/SolrObject.ts
+++ meta/interface/SolrObject.ts
@@ -14,16 +14,16 @@
  * }
  */
 export interface SolrObject {
-	id: string;
-	title: string;
-	creator: string[];
-	description: string;
-	index_year: string[]; //this is the year attribute of the original SolrObject
-	metadata_version: string;
-	modified: string;
-	access_rights: string[];
-	resource_class: string[];
-	meta: { [key: string]: string | string[] };
-	years: Set<string>;
-	parents?: string[];
+  id: string;
+  title: string;
+  creator: string[];
+  description: string;
+  index_year: string[]; //this is the year attribute of the original SolrObject
+  metadata_version: string;
+  modified: string;
+  access_rights: string[];
+  resource_class: string[];
+  meta: { [key: string]: string | string[] };
+  years: Set<string>;
+  parents?: string[];
 }
