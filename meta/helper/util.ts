c992760cecb352d4572ff155e8621e4714590755
Adam Cox <mr.adamcox@gmail.com>
2024-03-12 10:19:17 -0500

    fix: lint

--- meta/helper/util.ts
+++ meta/helper/util.ts
@@ -19,12 +19,12 @@ export function findFirstSentence(paragraph: string): string | undefined {
  * @returns: the key that matches the metadata in the schema folder, otherwise return the raw key
  */
 export function schemaMatch(rawSolrKey: string, schema_json: {}): string {
-	let result = Object.keys(schema_json).find(
-		(Key: string) => schema_json[Key].uri === rawSolrKey
-	);
-	if (result) {
-		return result;
-	} else return rawSolrKey;
+  let result = Object.keys(schema_json).find(
+    (Key: string) => schema_json[Key].uri === rawSolrKey
+  );
+  if (result) {
+    return result;
+  } else return rawSolrKey;
 }
 
 /**
@@ -33,8 +33,7 @@ export function schemaMatch(rawSolrKey: string, schema_json: {}): string {
  * @returns the raw attribute name that should be put to solr query
  */
 export function findSolrAttribute(json_key: string, schema_json: {}): string {
-	return Object.keys(schema_json).find((e) => e === json_key)
-		? schema_json[json_key]["uri"]
-		: json_key;
+  return Object.keys(schema_json).find((e) => e === json_key)
+    ? schema_json[json_key]["uri"]
+    : json_key;
 }
-
