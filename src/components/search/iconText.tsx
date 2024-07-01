206ca5d6096f87f54fb01c22ecc9848d0e5e6397
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2024-07-01 15:14:13 -0500

    #172: add result list item component

---
+++ src/components/search/iconText.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+interface Props {
+  svgIcon: any;
+  label: string;
+  labelClass: string;
+  labelColor: string;
+  roundBackground: boolean;
+}
+const IconText: React.FC<Props> = ({
+  svgIcon,
+  label,
+  labelClass,
+  labelColor,
+  roundBackground,
+}): JSX.Element => {
+  return (
+    <div className="flex items-center shadow-none">
+      {roundBackground ? (
+        <div
+          className={`relative w-10 h-10 mr-2 flex items-center justify-center bg-white rounded-full`}
+        >
+          <Image src={svgIcon} alt="Icon" className="w-6 h-6" />
+        </div>
+      ) : (
+        // for single icon without background
+        <Image src={svgIcon} alt="Icon" className="w-6 h-6" />
+      )}
+      <span className={`${labelClass} truncate`} style={{ color: labelColor }}>
+        {label}
+      </span>
+    </div>
+  );
+};
+
+export default IconText;
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,7 @@ module.exports = {
     fontSize: {
       sm: "0.8rem",
       base: "1rem",
+      "l": "1.125rem",
       xl: "1.25rem",
       "2xl": "1.563rem",
       "3xl": "1.953rem",
