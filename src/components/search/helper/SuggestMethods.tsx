1c2b5afe5022c0e58572acb059ad669ae48ba6f6
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2024-10-25 15:26:07 -0500

    update localization checking method

--- src/components/search/helper/SuggestMethods.tsx
+++ src/components/search/helper/SuggestMethods.tsx
@@ -1,11 +1,68 @@
 export const isLocationTerm = (suggest) => {
+  const stateNames = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming",
+  ];
+
+  const statePattern = new RegExp(
+    `^[A-Za-z\\s.'()-]+ (${stateNames.join("|")})$`
+  );
+
   return (
     /^[A-Za-z\s.'()-]+,\s[A-Za-z\s]+$/.test(suggest.term) ||
-    /^Southeast coastal Alaska$/.test(suggest.term) ||
+    statePattern.test(suggest.term) || // for South coast Alaska
     /^(North|South|West|East)\s[A-Za-z\s]+$/.test(suggest.term)
   );
 };
 
-export const containsYear = (suggest) => {
-  return /\(\d{4}\)/.test(suggest.payload);
+export const containsYear = (text) => {
+  return /\([12][0-9]{3}\)/.test(text);
 };
