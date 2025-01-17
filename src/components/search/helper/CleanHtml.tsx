ac384cdce41910503ad4c2d264cba6cd40ca5b9b
Wendy(Pengyin) Shan <wendy.shan@hotmail.com>
2025-01-17 15:56:17 -0500

    remove unused method

--- src/components/search/helper/CleanHtml.tsx
+++
@@ -1,5 +0,0 @@
-export const stripHtmlTags = (html: string | null | undefined) => {
-  if (!html) return "";
-  const doc = new DOMParser().parseFromString(html, "text/html");
-  return doc.body.textContent || "";
-};
