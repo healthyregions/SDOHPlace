6f2c46052efda85c46f7f575795954b4cf39c583
Sara Lambert <lambert8@illinois.edu>
2025-02-14 14:47:00 -0600

    fix: forgot to include enum

---
+++ src/lib/event.ts
@@ -0,0 +1,28 @@
+// Simple enum class describing Plausible event types
+
+/*
+Events being tracked:
+      - User clicks the "getting started" link
+      - User engages a spatial resolution filter
+      - User engages a theme filter
+      - User performs keyword search
+      - User switches to AI search mode
+      - User performs AI search
+      - User selects an item in the map-based location search (i.e. performs the action that selects a place and then zooms in the map to it)
+      - User clicks the "go to resource" button
+      - User clicks the "share" button
+      - User's search returns "no results"
+ */
+
+export enum EventType {
+  ClickedGetStarted = "Clicked.GetStarted",
+  ChangedSpatialResolution = "Changed.SpatialResolution",  // props => send new value(s)?
+  ChangedThemeFilter = "Changed.ThemeFilter",              // props => send new value(s)?
+  SubmittedKeywordSearch = "Submitted.KeywordSearch",      // props => send search query?
+  SubmittedChatSearch = "Submitted.ChatSearch",            // props => send search query?
+  ChangedSearchMode = "Changed.SearchMode",                // props => send new value(s)?
+  SubmittedLocationSearch = "Submitted.LocationSearch",    // props => send search query?
+  ClickedGoToResource = "Clicked.GoToResource",            // props => send resource name?
+  ClickedShareButton = "Clicked.ShareButton",              // props => send resource name?
+  ReceivedNoSearchResults = "Received.NoSearchResults",    // props => send search query?
+}
