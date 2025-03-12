1ec29bb15ffc19e74b6ae38de51eb541347b8c9d
Sara Lambert <lambert8@illinois.edu>
2025-03-12 12:58:31 -0500

    style: search/introCard somehow missed committing the extra padding for the right side

--- src/components/search/detailPanel/introCard.tsx
+++ src/components/search/detailPanel/introCard.tsx
@@ -49,7 +49,7 @@ const IntroCard = (props: Props): JSX.Element => {
               : ""}
           </div>
         </div>
-        <div className="flex-1 w-full sm:w-1/3 sm:pl-6 sm:pr-2.25 sm:pt-5 bg-lightbisque">
+        <div className="flex-1 w-full sm:w-1/3 sm:pl-6 p-5 bg-lightbisque">
           <div className={`${classes.introCard}`}>
             <b>Year:</b>{" "}
             {props.resultItem.index_year
