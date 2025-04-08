e844bc7558ceb4c4c6e6eb2d9f5284d94d617ada
Sara Lambert <lambert8@illinois.edu>
2025-04-08 16:14:02 -0500

    fix: forgot to include a couple necessary files

---
+++ src/components/guides/GuidesList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { GuidesContent } from "../../lib/guides";
+import GuidesItem from "./GuidesItem";
+import Pagination from "../news/Pagination";
+
+type Props = {
+  posts: GuidesContent[];
+  pagination: {
+    current: number;
+    pages: number;
+  };
+};
+export default function GuidesList({ guides, pagination }: Props) {
+  return (
+    <>
+      <div className={"post-list"}>
+        <ul className={""}>
+          {guides.map((it, i) => (
+            <li key={i}>
+              <GuidesItem item={it} />
+            </li>
+          ))}
+        </ul>
+        {/* <Pagination
+          current={pagination.current}
+          pages={pagination.pages}
+          link={{
+            href: (page) => (page === 1 ? "/news" : "/news/page/[page]"),
+            as: (page) => (page === 1 ? null : "/news/page/" + page),
+          }}
+        /> */}
+      </div>
+    </>
+  );
+}
