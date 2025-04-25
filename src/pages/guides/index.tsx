cd0645b680bf87eccf8275870e6a92eb84cf3145
Marynia <marynia.kolak@gmail.com>
2025-04-25 10:54:30 -0500

    update text

--- src/pages/guides/index.tsx
+++ src/pages/guides/index.tsx
@@ -35,14 +35,17 @@ export default function Index({ guides, tags, pagination }: Props) {
           <TwitterCardMeta url={url} title={title} />
           */}
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Proin et erat pellentesque augue consequat
-            tincidunt vulputate ut metus. In mollis, dolor a
-            molestie gravida, diam nunc tincidunt quam, id
-            faucibus magna nisi sollicitudin velit. Sed mollis bibendum ullamcorper. Proin placerat
-            dolor sed sagittis maximus. Pellentesque habitant
-            morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas.
+          While there exists a long and complex history studying
+          the role of place, social context, and the built environment within health,
+          current and common frameworks of the “social determinants of health” (SDoH)
+          tend to be simplistic in application. The SDOH & Place Project works to build
+          a community of practice around defining & using community-level social
+          determinants of health. To support these efforts, SDOH Research Guides help
+          focus and refine best practices and approaches in measuring distinct SDOH
+          indicators or structural drivers of health at community or regional scales.
+          </p><br />
+          <p>
+          The SDOH Guides are written by new and emerging scholars across the fields of SDOH research (ex. geography, social science, public health). Each guide takes a view on some SDOH topic, breaking down the jargon to clarify understanding and provide direct recommendations. The SDOH Guides will continue to be updated and changed, according to research directions that unfold.
           </p>
         </Grid>
 
