a9bb8ba41f2946e032366dec6a3753ca8e51bf59
Sara Lambert <lambert8@illinois.edu>
2025-07-01 12:38:03 -0500

    refactor: reworked News page into smaller components to make it more readable

--- src/components/news/PostList.tsx
+++ src/components/news/PostList.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { PostContent } from "../../lib/posts";
+import { PostContent } from "@/lib/posts";
 import PostItem from "./PostItem";
 import TagLink from "./TagLink";
 import Pagination from "./Pagination";
-import { TagContent } from "../../lib/tags";
+import { TagContent } from "@/lib/tags";
 import {Grid} from "@mui/material";
 import Link from "next/link";
 
@@ -15,47 +15,56 @@ type Props = {
     pages: number;
   };
 };
+
+const TagsList = ({ tags }) => <>
+  <strong>Tags</strong>
+  <ul className={"categories"}>
+    {tags.map((it, i) => (
+      <li key={i}>
+        <TagLink tag={it} />
+      </li>
+    ))}
+  </ul>
+</>;
+
+const PostsTable = ({ className, posts, pagination }) => <>
+  <ul className={className}>
+    {posts.map((it, i) => (
+      <li key={i}>
+        <PostItem post={it} />
+      </li>
+    ))}
+  </ul>
+  <Pagination
+    current={pagination.current}
+    pages={pagination.pages}
+    link={{
+      href: (page) => (page === 1 ? "/news" : "/news/page/[page]"),
+      as: (page) => (page === 1 ? null : "/news/page/" + page),
+    }}
+  />
+</>
+
 export default function PostList({ posts, tags, pagination }: Props) {
   return (
-    <>
-      <div className={"post-list"}>
-        <Grid container spacing={0} className={'mb-8'}>
-          <Grid item xs={10}>
-            Here you can find information pertaining to events, calls for applications/interest,
-            critical updates and information, and other news centered around the SDOH & Place Project.
-            If you’d like to stay more up to date, subscribe to our monthly
-            {" "}<Link href={'https://groups.webservices.illinois.edu/subscribe/192463'} target={'_blank'} rel={'noreferrer noopener'}>
-            Newsletter
-          </Link>.
-          </Grid>
+    <div className={"post-list"}>
+      <Grid container spacing={0}>
+        <Grid item xs={10}>
+          Here you can find information pertaining to events, calls for applications/interest,
+          critical updates and information, and other news centered around the SDOH & Place Project.
+          If you’d like to stay more up to date, subscribe to our monthly
+          {" "}<Link href={'https://groups.webservices.illinois.edu/subscribe/192463'} target={'_blank'} rel={'noreferrer noopener'}>
+          Newsletter</Link>.
+
+          <PostsTable className={'mt-8'} posts={posts} pagination={pagination}></PostsTable>
         </Grid>
 
-        <ul className={""}>
-          {posts.map((it, i) => (
-            <li key={i}>
-              <PostItem post={it} />
-            </li>
-          ))}
-        </ul>
-        <Pagination
-          current={pagination.current}
-          pages={pagination.pages}
-          link={{
-            href: (page) => (page === 1 ? "/news" : "/news/page/[page]"),
-            as: (page) => (page === 1 ? null : "/news/page/" + page),
-          }}
-        />
-      </div>
-      <ul className={"categories"}>
-        <li>
-          <strong>Tags</strong>
-        </li>
-        {tags.map((it, i) => (
-          <li key={i}>
-            <TagLink tag={it} />
-          </li>
-        ))}
-      </ul>
-    </>
+        <Grid item xs={1}>{/* Intentional gap, since this version of Grid may not support "offset" */}</Grid>
+
+        <Grid item xs={1}>
+          <TagsList tags={tags}></TagsList>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
