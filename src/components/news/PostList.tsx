723b7bb3142d96d1630f4dbd7e7fb1cb5b5da2b3
Sara Lambert <lambert8@illinois.edu>
2025-07-01 14:17:57 -0500

    fix: News page - mobile display of Tags list

--- src/components/news/PostList.tsx
+++ src/components/news/PostList.tsx
@@ -16,16 +16,18 @@ type Props = {
   };
 };
 
-const TagsList = ({ tags }) => <>
+const TagsList = ({ tags }) => <div className={' my-4 flex flex-col'}>
   <strong>Tags</strong>
-  <ul className={"categories"}>
+  {/* Mobile: Display tags as a horizontal row  */}
+  {/* Desktop: Display tags as a vertical list  */}
+  <ul className={"flex flex-col max-md:flex-row"}>
     {tags.map((it, i) => (
-      <li key={i}>
+      <li key={i} className={'mr-8'}>
         <TagLink tag={it} />
       </li>
     ))}
   </ul>
-</>;
+</div>;
 
 const PostsTable = ({ className, posts, pagination }) => <>
   <ul className={className}>
@@ -49,22 +51,22 @@ export default function PostList({ posts, tags, pagination }: Props) {
   return (
     <div className={"post-list"}>
       <Grid container spacing={0}>
-        <Grid item xs={10}>
+        <Grid item xs={12} md={10}>
           Here you can find information pertaining to events, calls for applications/interest,
           critical updates and information, and other news centered around the SDOH & Place Project.
           If you’d like to stay more up to date, subscribe to our monthly
           {" "}<Link href={'https://groups.webservices.illinois.edu/subscribe/192463'} target={'_blank'} rel={'noreferrer noopener'}>
           Newsletter</Link>.
-
-          <PostsTable className={'mt-8'} posts={posts} pagination={pagination}></PostsTable>
         </Grid>
 
-        <Grid item xs={1}>{/* Intentional gap, since this version of Grid may not support "offset" */}</Grid>
+        <Grid item md={1}>{/* Intentional gap, since this version of Grid may not support "offset" */}</Grid>
 
-        <Grid item xs={1}>
+        <Grid item xs={12} md={1}>
           <TagsList tags={tags}></TagsList>
         </Grid>
       </Grid>
+
+      <PostsTable className={'mt-8'} posts={posts} pagination={pagination}></PostsTable>
     </div>
   );
 }
