dd7b9b184981efbb5dd031c65d373ab086ec47dc
Sara Lambert <lambert8@illinois.edu>
2025-07-18 14:02:15 -0500

    fix: default className to empty string (again, React should just include this always)

--- src/components/news/Date.tsx
+++ src/components/news/Date.tsx
@@ -1,6 +1,6 @@
 import { format, formatISO } from "date-fns";
 
-export default function Date({ className, date }) {
+export default function Date({ className = '', date }) {
   return (
     <time dateTime={formatISO(date)} className={className}>
       <span>{format(date, "LLLL d, yyyy")}</span>
