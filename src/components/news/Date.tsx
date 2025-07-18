6b2337e6b73e374a4d2fe67e3b49dcb689787d6e
Sara Lambert <lambert8@illinois.edu>
2025-07-18 13:52:31 -0500

    fix: className should really just be built-in at this point

--- src/components/news/Date.tsx
+++ src/components/news/Date.tsx
@@ -3,9 +3,9 @@ import { format, formatISO } from "date-fns";
 type Props = {
   date: Date;
 };
-export default function Date({ date }: Props) {
+export default function Date({ className, date }: Props) {
   return (
-    <time dateTime={formatISO(date)}>
+    <time dateTime={formatISO(date)} className={className}>
       <span>{format(date, "LLLL d, yyyy")}</span>
       <style jsx>
         {`
