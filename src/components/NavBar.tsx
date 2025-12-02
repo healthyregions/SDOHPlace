1400e14c8ba6c1252a6270fbf28a4f4e8361d824
Adam Cox <mr.adamcox@gmail.com>
2025-12-02 11:30:41 -0600

    style: add external link option in navbar, fix styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { makeStyles } from "@mui/styles";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight';
+import OpenInNew from '@mui/icons-material/OpenInNew';
 
 const useStyles = makeStyles((theme) => ({
   mobileHamburgerMenu: {
@@ -58,7 +59,12 @@ function NavDropdownButton({ title, dropdownElId, items, directLink }: Props) {
       >
         {items.map((item, index) => (
           <li key={index}>
-            <Link href={item.url} target={item.target || ''}>{item.subitem && <SubdirectoryArrowRightIcon />}{item.title}</Link>
+            <Link href={item.url} target={item.target || ''}>
+              <span>
+                {item.subitem && <SubdirectoryArrowRightIcon style={{height:".9em"}} />}
+              </span>
+              {item.title}
+              {item.target =="_blank" && <OpenInNew style={{height:".75em"}} />}</Link>
           </li>
         ))}
       </ul>
