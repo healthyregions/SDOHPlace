b63500cfd9c34dc0f574c1d9d07f76d19ebb2c2f
Sara Lambert <lambert8@illinois.edu>
2026-09-14 13:40:53 -0500

    style: polish existing view, add toggle between Grid/List views

--- src/components/posters/PostersList.tsx
+++ src/components/posters/PostersList.tsx
@@ -1,19 +1,12 @@
 import React, {useState} from "react";
 import type { PostersContent } from "../../lib/posters";
 import PostersItem from "./PostersItem";
-import Pagination from "../news/Pagination";
 import Grid from "@mui/material/Grid";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
-import Link from "next/link";
-import GuidesList from "@/components/guides/GuidesList";
-import styled from "@emotion/styled";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-
-const TagsGroup = styled(ButtonGroup)`
-    color: #7e1cc4;
-`;
+import {BsGrid3X3GapFill, BsList} from "react-icons/bs";
 
 type Props = {
   posters: PostersContent[];
@@ -24,28 +17,51 @@ type Props = {
 };
 export default function PostersList({ posters, pagination }: Props) {
   const [poster, setPoster] = useState(undefined);
-  const [tag, setTag] = useState(undefined);
-  const tags = [...new Set(posters?.flatMap(p => p?.tags))];
-
-
+  //const [tag, setTag] = useState(undefined);
+  //const tags = [...new Set(posters?.flatMap(p => p?.tags))];
+  const [view, setView] = useState('grid');
 
   return (
     <>
       <div className={"post-list"}>
-        {/* Future: separate posters for different events using Tags */}
-        {/*<Grid container spacing={2}>*/}
-        {/*  <ButtonGroup variant="outlined" aria-label="Basic button group">*/}
-        {/*    {*/}
-        {/*      tags?.map((t, index) =>*/}
-        {/*        <Button color={'secondary'} style={{ color: '#7e1cc4' }} key={`tag-${index}`} onClick={() => setTag(t)}>{t}</Button>*/}
-        {/*      )*/}
-        {/*    }*/}
-        {/*  </ButtonGroup>*/}
-        {/*</Grid>*/}
+        <Grid container spacing={2} alignItems={'center'} justifyContent={'space-between'} marginBottom={'2rem'}>
+          {/* Future: Separate posters for different events using Tags */}
+          {/*<ButtonGroup variant="outlined" aria-label="Basic button group">*/}
+          {/*  {*/}
+          {/*    tags?.map((t, index) =>*/}
+          {/*      <Button size={'small'} style={{color:'#7e1cc4'}} color={'secondary'} key={`tag-${index}`} onClick={() => setTag(t)}>{t}</Button>*/}
+          {/*    )*/}
+          {/*  }*/}
+          {/*</ButtonGroup>*/}
 
-        {/* Option A: ImageList - more concise/clean, but hides author info behind a manual click */}
-        <Grid container spacing={30}>
-          <Grid size={{ xs: poster ? 6 : 12 }}>
+          {/* Toggle between Grid vs List view */}
+          <ButtonGroup aria-label="Grid or List View">
+            <Button color={'secondary'}
+                    onClick={() => setView('grid')}
+                    aria-label={'Grid view'}
+                    style={{
+                      border: '1px solid #7e1cc4',
+                      color: view === 'grid' ? 'white' : '#7e1cc4',
+                      backgroundColor: view === 'grid' ? '#7e1cc4' : 'white',
+                    }}>
+              <BsGrid3X3GapFill />
+            </Button>
+            <Button color={'secondary'}
+                    onClick={() => setView('list')}
+                    aria-label={'List view'}
+                    style={{
+                      border: '1px solid #7e1cc4',
+                      color: view === 'list' ? 'white' : '#7e1cc4',
+                      backgroundColor: view === 'list' ? '#7e1cc4' : 'white',
+                    }}>
+              <BsList />
+            </Button>
+          </ButtonGroup>
+        </Grid>
+
+        {/* Grid view: ImageList - more concise/clean, but hides author info behind a manual click */}
+        {view === 'grid' && <Grid container spacing={30} alignItems={'center'}>
+          <Grid size={6}>
             <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
               {posters.map((item, index) => (
                 <ImageListItem key={`poster-image-${index}`}
@@ -61,26 +77,33 @@ export default function PostersList({ posters, pagination }: Props) {
                     src={`${item.image}?w=164&h=164&fit=crop&auto=format`}
                     alt={item.title}
                     loading="lazy"
+                    draggable="false"
                   />
                 </ImageListItem>
               ))}
             </ImageList>
           </Grid>
-          {
-            poster && <Grid size={{ xs: 6 }}>
-              <PostersItem item={poster}></PostersItem>
-            </Grid>
-          }
-        </Grid>
+          <Grid size={6}>
+            {
+              poster && <PostersItem item={poster}></PostersItem>
+            }
+            {
+              !poster && <Grid container justifyContent={'end'}>
+                Choose a poster to learn more
+              </Grid>
+            }
+          </Grid>
+        </Grid>}
 
-        {/* Option B: Manual Grid - more closely matches our styles, but feels wordy */}
-        <Grid container spacing={2}>
-          {posters?.filter(p => !tag || p.tags?.includes(tag))?.map((it, i) =>
-            <Grid key={`poster-${i}`} size={{ xs: 12, md: 6 }}>
+        {/* List view: more closely matches our styles, but feels wordy */}
+        {view === 'list' && <Grid container spacing={2}>
+          {/* TODO - filter by tag: .filter(p => !tag || p.tags?.includes(tag))? */}
+          {posters?.map((it, i) =>
+            <Grid key={`poster-${i}`} size={12}>
               <PostersItem item={it} />
             </Grid>
           )}
-        </Grid>
+        </Grid>}
         {/* <Pagination
           current={pagination.current}
           pages={pagination.pages}
